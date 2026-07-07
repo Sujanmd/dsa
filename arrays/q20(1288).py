5e3a23237358643dd2c33ab00b5c3c234d3f7b85
Sujanmd <smdsujanmd@gmail.com>
2026-07-07 13:15:36 +0530

    final commit

---
+++ arrays/q20(1288).py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: (x[0],-x[1]))
+        low=intervals[0][0]
+        high=intervals[0][1]
+        ans=len(intervals)
+        for i in range(1,len(intervals)):
+            if intervals[i][0]==low or intervals[i][1]<=high:
+                ans-=1
+            else: 
+                low=intervals[i][0]
+                high=intervals[i][1]
+        return ans
+        
